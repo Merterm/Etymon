b07ac20a5854032479fd2cbbf9b26f27a3d6a09b
Utku Uckun <utku.uckun@gmail.com>
2018-02-21 22:37:57 +0300

    Nisanyansozluk
    First working version. May require debugging.

---
+++ code/Nisanyansozluk.java
@@ -0,0 +1,77 @@
+package Websites;
+
+import java.io.PrintWriter;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Nisanyansozluk {
+
+	public static void main(String[] args) {
+
+
+		//Properties
+		String link = "http://www.nisanyansozluk.com/?k=";
+		String currentWord = "aba";
+		String follow = "&view=annotated";
+		boolean hasMoreWords = true;
+		Elements links = null;
+		Elements linksInCurrentWord = null;
+		Elements ElementsInCurrentWord = null;
+		Element currentLink = null;
+		
+		while(hasMoreWords){
+			PrintWriter out = null;
+			try {
+				links = Jsoup.connect(link + currentWord + follow).get().select("a[href]");
+				currentLink = Jsoup.connect(link + currentWord + follow).get().getElementsByAttributeValue("title",currentWord).get(0);
+				ElementsInCurrentWord = currentLink.parent().children();
+
+				linksInCurrentWord = ElementsInCurrentWord.select("a[href]");
+				//writing html to file
+				out = new PrintWriter(currentWord + ".html");
+				String e = ElementsInCurrentWord.html(); 
+				out.write(e);
+				out.close();
+
+			} catch (Exception e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			}
+
+
+			//Calculating if has more words
+			if(currentWord.compareTo("zürriyet") == 0)
+				hasMoreWords = false;
+			else
+			{
+				//get rid of trash links
+				for(int i = 0; i < 16; i++)
+				{
+					links.remove(0);
+				}
+				
+				int index = 0;
+				boolean found = false;
+				while(!found)
+				{
+					if(links.get(index).text().compareTo(currentLink.child(0).text()) == 0)
+						found = true;
+					else
+						index++;
+				}
+				
+				int internalLinkCount = linksInCurrentWord.size();
+				int offset = index + internalLinkCount;
+				
+				currentWord = links.get(offset).ownText();
+				System.out.println("Done");
+			}
+
+
+		}
+
+	}
+
+}
