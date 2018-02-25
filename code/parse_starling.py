5703694a01bd4f0937e9c7da505f54726359b1f8
Merterm <mertincek@hotmail.com>
2018-02-25 17:30:51 +0300

    starling extraction update

--- code/parse_starling.py
+++ code/parse_starling.py
@@ -6,52 +6,59 @@ import os
 #Open a csv file to write all words and their etymologies
 csvfile = open('starling_etymologies.csv', 'wb')
 csvwriter = csv.writer(csvfile, delimiter=';')
-csvwriter.writerow(['word','etymology'])
+csvwriter.writerow(['language family','word','etymology'])
 csvfile.close()
 
 cnt = 0
-#Iterate over every html document in directory
-for html in os.listdir("../data/starling/Turkic Etymology/"):
-    #print str(html)
+for folder in os.listdir("../data/starling/"):
     try:
-        #Open HTML file
-        html_file = open("../data/starling/Turkic Etymology/" + html.decode('utf-8'), "r")
-        content = html_file.read()
-        soup = BeautifulSoup(content.decode('utf-8'), 'html.parser')
+        #Iterate over every html document in directory
+        for html in os.listdir("../data/starling/" + str(folder) + "/"):
+            #print str(html)
+            try:
+                #Open HTML file
+                html_file = open("../data/starling/" + str(folder) + "/" + html.decode('utf-8'), "r")
+                content = html_file.read()
+                soup = BeautifulSoup(content.decode('utf-8'), 'html.parser')
 
-        #Find the records
-        records = soup.find_all("div", class_="results_record")
+                #Find the records
+                records = soup.find_all("div", class_="results_record")
 
-        for record in records:
-            cnt = cnt + 1
-            conts = record.contents
+                for record in records:
+                    cnt = cnt + 1
+                    conts = record.contents
 
-            #Find the word
-            word = conts[0].find(class_="unicode").string
-            #print word
+                    #Find language family
+                    family = conts[0].find("font", color="green").string
 
-            #Find the etymologies
-            etymology = []
-            for i in range(2,len(conts)-1):
-                text = conts[i].find(class_="fld").string
-                if (text == "Meaning:") or (text == "Russian meaning:") or \
-                (text == "Comments:"):
-                    pass
-                else:
-                    unic = conts[i].find(class_="unicode").string
-                    if not unic:
-                        etymology.append( (text, conts[i].find(class_="unicode").contents) )
-                    else:
-                        etymology.append((text, unic))
+                    #Find the word
+                    word = conts[0].find(class_="unicode").string
+                    #print word
 
-            #Write to CSV
-            csvfile = open('starling_etymologies.csv', 'ab')
-            csvwriter = csv.writer(csvfile, delimiter=';')
-            csvwriter.writerow([word, etymology])
-            csvfile.close()
+                    #Find the etymologies
+                    etymology = []
+                    for i in range(2,len(conts)-1):
+                        text = conts[i].find(class_="fld").string
+                        if (text == "Meaning:") or (text == "Russian meaning:") or \
+                        (text == "Comments:"):
+                            pass
+                        else:
+                            unic = conts[i].find(class_="unicode").string
+                            if not unic:
+                                etymology.append( (text, conts[i].find(class_="unicode").contents) )
+                            else:
+                                etymology.append((text, unic))
 
-        html_file.close()
+                    #Write to CSV
+                    csvfile = open('starling_etymologies.csv', 'ab')
+                    csvwriter = csv.writer(csvfile, delimiter=';')
+                    csvwriter.writerow([family, word, etymology])
+                    csvfile.close()
+
+                html_file.close()
+            except:
+                print "passing " + str(html)
     except:
-        print "passing " + str(html)
+        print "passing " + str(folder)
 
 print "Total word count: " + str(cnt)
