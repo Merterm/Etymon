9ed9ad4128d6b626210bf0b7b0a1feef64fbd6a5
Merterm <mertincek@hotmail.com>
2018-04-01 20:41:15 +0300

    More node.js and Website Update 🍁

--- code/Website/node-etymon/server.js
+++ code/Website/node-etymon/server.js
@@ -4,6 +4,7 @@ let child;
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+var cytoscape = require('cytoscape');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -30,6 +31,46 @@ app.post('/', function (req, res) {
       let etymology = stdout;
       res.render('index', {etymology: stdout, error: null});
     }
+  });
+
+  var cy = cytoscape({
+
+          elements: [ // list of graph elements to start with
+            { // node a
+              data: { id: 'a' }
+            },
+            { // node b
+              data: { id: 'b' }
+            },
+            { // edge ab
+              data: { id: 'ab', source: 'a', target: 'b' }
+            }
+          ],
+
+          style: [ // the stylesheet for the graph
+            {
+              selector: 'node',
+              style: {
+                'background-color': '#666',
+                'label': 'data(id)'
+              }
+            },
+
+            {
+              selector: 'edge',
+              style: {
+                'width': 3,
+                'line-color': '#ccc',
+                'target-arrow-color': '#ccc',
+                'target-arrow-shape': 'triangle'
+              }
+            }
+          ],
+
+          layout: {
+            name: 'grid',
+            rows: 1
+          }
   });
 })
 
