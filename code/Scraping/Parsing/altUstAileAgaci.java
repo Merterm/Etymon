b336b5771727222ad497bf592c76c09ba78ef634
Utku U. <utku.uckun@gmail.com>
2018-04-01 18:38:10 +0300

    Looks up parent, children and related words of a given word from Etymwn.tsv.

--- code/Scraping/Parsing/altUstAileAgaci.java
+++ code/Scraping/Parsing/altUstAileAgaci.java
@@ -1,32 +1,58 @@
 package Scraping.Parsing;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class altUstAileAgaci {
 
-	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException {	//this won't be a main function in the future!
-		//Properties
-		BufferedReader br = null;
-		String input = "closet"; 	//Get this as input in final version
-		ArrayList<String> lines = new ArrayList();
-	    br = new BufferedReader(new FileReader("W:\\Etymon\\data\\etymwn.tsv"));	//Change it on your computer
-	
-		 
-		 String currLine = br.readLine();
-		 while(currLine != null)
-		 {
-			 if(currLine.contains(input))
-				 lines.add(currLine);
-			 
-			 currLine = br.readLine();
-		 }
-		 for(int i = 0; i < lines.size();i++)
-			 System.out.println(lines.get(i));
+		getRelations("etymology");
 	}
 
+	public static void getRelations(String input) throws IOException 
+	{
+		//Properties
+		String EOO = "etymological_origin_of";
+		String E = "etymology";
+		String HDF = "has_derived_form";
+		BufferedReader br = null;
+//		String word = ": "; 	//Get this as input from user in final version
+//		word = word.concat(input.toLowerCase());
+//		word = word.concat("\t");
+		ArrayList<String> parents = new ArrayList<String>();
+		ArrayList<String> children = new ArrayList<String>();
+		ArrayList<String> related = new ArrayList<String>();
+		//ArrayList<String> lines = new ArrayList<String>();
+		br = new BufferedReader(new FileReader("W:\\Downloads\\etymwn\\etymwn.tsv"));	//Change it on your computer
+
+
+		String currLine = br.readLine();
+		while(currLine != null)
+		{
+			String word = currLine.substring(currLine.indexOf(" ")+1, currLine.indexOf("\t"));
+			if(word.compareTo(input) == 0)			
+			{
+				int position = currLine.lastIndexOf(':');
+				int relPos = currLine.indexOf("rel:") + 4;
+				String temp = currLine.substring(relPos);
+				String relType = temp.substring(0, temp.indexOf('\t'));
+				 
+				
+				if(relType.compareTo(E) == 0)	//Extract parents of the words			
+					parents.add(currLine.substring(position+2));
+				else
+					if(relType.compareTo(EOO) == 0)
+						children.add(currLine.substring(position+2));				
+					else
+						if(relType.compareTo(HDF) == 0)
+							related.add(currLine.substring(position+2));
+			}
+			currLine = br.readLine();
+
+		}
+		br.close();
+	}
 }
+
