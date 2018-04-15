4a6aed5bf4f4406d5a05cb87fda0cdd173f43fae
Merterm <mertincek@hotmail.com>
2018-04-15 16:04:29 +0300

    LSTM Working 🦔

--- code/Machine-Learning/lstm.py
+++ code/Machine-Learning/lstm.py
@@ -22,7 +22,7 @@ print "Total Characters: ", n_chars
 print "Total Vocab: ", n_vocab
 
 # prepare the dataset of input to output pairs encoded as integers
-seq_length = 100
+seq_length = 40
 dataX = []
 dataY = []
 for i in range(0, n_chars - seq_length, 1):
@@ -50,35 +50,8 @@ model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 # 3. Train the network
 # define the checkpoint
-filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+filepath="weights-improvement-{epoch:02d}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 
 model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
-
-# 4. Generate text
-# load the network weights
-filename = "weights-improvement-19-1.9435.hdf5"
-model.load_weights(filename)
-model.compile(loss='categorical_crossentropy', optimizer='adam')
-
-#Reverse int to char
-int_to_char = dict((i, c) for i, c in enumerate(chars))
-
-# pick a random seed
-start = numpy.random.randint(0, len(dataX)-1)
-pattern = dataX[start]
-print "Seed:"
-print "\"", ''.join([int_to_char[value] for value in pattern]), "\""
-# generate characters
-for i in range(1000):
-	x = numpy.reshape(pattern, (1, len(pattern), 1))
-	x = x / float(n_vocab)
-	prediction = model.predict(x, verbose=0)
-	index = numpy.argmax(prediction)
-	result = int_to_char[index]
-	seq_in = [int_to_char[value] for value in pattern]
-	sys.stdout.write(result)
-	pattern.append(index)
-	pattern = pattern[1:len(pattern)]
-print "\nDone."
