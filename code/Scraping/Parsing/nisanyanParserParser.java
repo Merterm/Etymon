a986b7a0a62305017bf87d8fc5ad4cc2a4231063
Utku U. <utku.uckun@gmail.com>
2018-04-17 23:16:28 +0300

    extracted_nisanyan parser updated.

--- code/Scraping/Parsing/nisanyanParserParser.java
+++ code/Scraping/Parsing/nisanyanParserParser.java
@@ -14,16 +14,16 @@ public class nisanyanParserParser {
 		// TODO Auto-generated method stub
 		BufferedReader BR = new BufferedReader(new InputStreamReader(new FileInputStream ("W:\\Etymon\\code\\Scraping\\Parsing\\extracted_nisanyan.csv"), "UTF-8" ));
 		PrintWriter PW = new PrintWriter(new File("W:\\Etymon\\code\\Scraping\\Parsing\\Nisanyan.tsv"));
-		
+
 		//Discarding the first line
 		String line = BR.readLine();
 
 		line = BR.readLine();
 		while(line != null)
 		{
-			if(!line.contains("none"))
+			if(!line.contains("none") && !(line.contains("<p>")|| line.contains("<b>") || line.contains("<sup>")|| line.contains("<i>") ))
 			{
-				
+
 				String prevWord;
 				String nextWord;
 				String prevLan;
@@ -34,35 +34,120 @@ public class nisanyanParserParser {
 				prevIndex = 0;
 				nextIndex = line.indexOf(';');
 				prevWord = line.substring(prevIndex, nextIndex);
-				line = line.substring(line.indexOf(',')+1);
+				line = line.substring(line.indexOf('\t')+1); //shortening the line
 				prevLan = "tur";
-				
+
 				while(line.length() > 3)
 				{
 					String newLine = "";
 					newLine = newLine.concat(prevLan + ": ");
 					newLine = newLine.concat(prevWord + '\t');
 					newLine = newLine.concat("rel: etymology" + '\t');
-					prevIndex = line.indexOf('\'') + 1;
+					prevIndex = 0;//line.indexOf('\t'') + 1;
 					comaIndex = line.indexOf(',');
-					nextIndex = line.indexOf("\')");
-					prevLan = line.substring(prevIndex, comaIndex-1);
-					newLine = newLine.concat(prevLan + ": ");
-					if(line.charAt(comaIndex + 3) == '\'' )
-						nextWord = line.substring(comaIndex + 4, nextIndex);
-					else
-					{
-						nextIndex = line.indexOf("\")");
-						nextWord = line.substring(comaIndex + 5, nextIndex-1);
-					}
-					newLine = newLine.concat(nextWord + '\n');
+					nextIndex = line.indexOf("\t");
+					prevLan = line.substring(prevIndex, comaIndex);
 					
-					line = line.substring(nextIndex+3);
+					boolean lanDefined = false;
+					switch(prevLan)
+					{
+					case "Fransýzca ":
+						prevLan = "fre";
+						lanDefined = true; 
+						break;
+					case "Arapça":
+						prevLan = "ara";
+						lanDefined = true; 
+						break;	
+					case "Türkçe":
+						prevLan = "tur";
+						lanDefined = true; 
+						break;
+					case "Latince":
+						prevLan = "lat";
+						lanDefined = true; 
+						break;
+					case "Farsça":
+						prevLan = "per";
+						lanDefined = true; 
+						break;
+					case "Ýngilizce ":
+						prevLan = "eng";
+						lanDefined = true; 
+						break;
+					case "Ýspanyolca ":
+						prevLan = "spa";
+						lanDefined = true; 
+						break;
+					case "Eski Yunanca":
+						prevLan = "grc";
+						lanDefined = true; 
+						break;
+					case "Almanca ":
+						prevLan = "ger";
+						lanDefined = true; 
+						break;
+					case "Ýtalyanca":
+						prevLan = "ita";
+						lanDefined = true; 
+						break;
+					case "Yeni Yunanca":
+						prevLan = "gre";
+						lanDefined = true; 
+						break;
+					case "Moðolca ":
+						prevLan = "mon";
+						lanDefined = true; 
+						break;
+					case "Rusça ":
+						prevLan = "rus";
+						lanDefined = true; 
+						break;
+					case "Ýbranice ":
+						prevLan = "heb";
+						lanDefined = true; 
+						break;
+					case "Ermenice":
+						prevLan = "arm";
+						lanDefined = true; 
+						break;
+					case "Eski Farsça ":
+						prevLan = "peo";
+						lanDefined = true; 
+						break;
+					case "Akatça":
+						prevLan = "akk";
+						lanDefined = true; 
+						break;
+					case "Fransýzca ve Ýngilizce ":
+						prevLan = "fre/eng";
+						lanDefined = true; 
+						break;
+					case "Venedikçe":
+						prevLan = "vec";
+						lanDefined = true; 
+						break;
+					default:
+						if(prevLan.compareTo("tur") != 0 && !lanDefined )
+							System.out.println(prevLan);
+						break;
+					}
+					newLine = newLine.concat(prevLan + ": ");
+//					if(line.charAt(comaIndex + 3) == '\'' )
+//						nextWord = line.substring(comaIndex + 4, nextIndex);
+//					else
+//					{
+						//nextIndex = line.indexOf("\")");
+						nextWord = line.substring(comaIndex + 1, nextIndex);
+					//}
+					newLine = newLine.concat(nextWord + '\n');
+
+					line = line.substring(nextIndex+1);
 					prevWord = nextWord;
 					PW.write(newLine);
 				}
-								
-				
+
+
 			}
 			line = BR.readLine();
 		}
