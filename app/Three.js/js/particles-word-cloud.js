57558b2772534621561e7cd51d46381224ec19fd
shompot <cholpon94@gmail.com>
2018-05-01 04:50:21 +0300

    connected words  and lines are shown

--- app/Three.js/js/particles-word-cloud.js
+++ app/Three.js/js/particles-word-cloud.js
@@ -1,14 +1,22 @@
 var container, stats, camera, controls, scene, renderer;
-var geometry, group;
+var geometry, group, material;
+var word, connectedWords, points;
+var numConnectedWords = 7, numConnectingLines = numConnectedWords;
 var particles, labels;
 var WIDTH, HEIGHT;
 var mouseX = 0, mouseY = 0, count = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 var firstAnimationTime = 50, startTime, lastRenderTime = 0, acceleration;
-var maxZ = 2000, minZ = 10, deltaZ = 0, deltaTime = 0, tCount = 0;
+var maxZ = 2000, minZ = 10, optimalZ = 20, state = 0;
+var c;
 
-document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+// state 0: start
+// state 1: first zoom in animation
+// state 2: lines
+// state 3: words
+
+//document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
 window.onload = function (){
     init();
@@ -16,7 +24,7 @@ window.onload = function (){
 }
 
 function init (){
-    geometry = new THREE.Geometry();
+    //geometry = new THREE.Geometry();
     container = document.getElementById('particles-container');
     // Set the scene size.
     WIDTH = window.innerWidth;
@@ -29,6 +37,8 @@ function init (){
 
     camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 10000);
     camera.position.z = maxZ;
+    //camera.position.x = 0;
+    //camera.position.y = 0;
 
     scene = new THREE.Scene();
     scene.add(camera);
@@ -41,42 +51,30 @@ function init (){
     controls = new THREE.OrbitControls( camera, renderer.domElement );
     controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
     controls.dampingFactor = 0.25;
-    controls.panningMode = THREE.HorizontalPanning; // default is THREE.ScreenSpacePanning
-    controls.minDistance = 100;
-    controls.maxDistance = 1600
+    //controls.panningMode = THREE.HorizontalPanning; // default is THREE.ScreenSpacePanning
+    //controls.panningMode = THREE.VerticalPanning;
+    controls.minDistance = minZ;
+    controls.maxDistance = maxZ;
     controls.maxPolarAngle = Math.PI / 2;
 
     // Attach the renderer-supplied DOM element
     container.appendChild(renderer.domElement);
 
     // CREATE SPRITES
-    var material = new THREE.SpriteMaterial( {
+    material = new THREE.SpriteMaterial( {
    	map: new THREE.CanvasTexture( generateSprite() ),
    	blending: THREE.AdditiveBlending
     } );
 
-    particles = new Array();
-    labels = new Array();
+    points = new Array ();
 
-    var i = 0;
+    // add the word
+    word = addWord(0,0);
+    scene.add( word );
 
-    particle = particles[ i ++ ] = new THREE.Sprite( material );
-
-    particle.position.x = 0;
-    particle.position.z = 0;
-    particle.rotation.x = Math.random() * 6;
-    particle.rotation.y = Math.random() * 6;
-    particle.rotation.z = Math.random() * 6;
-
-    scene.add( particle );
-
-/*
-    // add event listeners
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-    document.addEventListener( 'touchmove', onDocumentTouchMove, false );
-*/
+    //addConnectedWords(8);
 	 window.addEventListener( 'resize', onWindowResize, false );
+    //startTime = Date.now();
 }
 function animate (){
     stats.update();
@@ -86,42 +84,66 @@ function animate (){
 
 function render(){
 
-   // new stuff
-   //var nowTime = Date.now();
-
-
-   //if (lastRenderTime == 0){
-      // set time for startAnimationTime
-      //startTime = Date.now();
-      //firstAnimationTime = 2000;
-      //console.log("Start1");
-   //}
-   //else {
-      //if (deltaTime == 0){
-         //console.log("Start2");
-         //deltaTime = nowTime - startTime;
-         //acceleration = 2*(maxZ-minZ)/(firstAnimationTime*firstAnimationTime);
-         //deltaZ = acceleration;
-         //console.log("Time delta "+ deltaTime);
-         //console.log("Z delta "+ deltaZ);
-         //console.log("t square "+ (firstAnimationTime*firstAnimationTime));
-      //}
-   //}
-
-   //lastRenderTime = nowTime;
-
-   if (camera.position.z > minZ) {
-      camera.position.z -= camera.position.z * 0.05;
-      //camera.position.z -= deltaZ;
-      //console.log("inside " + camera.position.z + " " + deltaZ);
-      //deltaZ = acceleration*deltaTime*tCount++;
-      //deltaZ = acceleration*(nowTime-startTime);
+   // zoom in animation
+   if (state === 0 && (camera.position.z - camera.position.z * 0.03) > minZ) {
+      camera.position.z -= camera.position.z * 0.03;
       camera.lookAt( scene.position );
+
+      if ((camera.position.z - camera.position.z * 0.03) <= minZ){
+         c = count;
+         state++;
+      }
    }
 
+   if (state === 1){
+      var d = count - c;
+      if (d > 0.6){
+         c = Math.round(count);
+         state ++;
+         addConnectedWords(numConnectedWords);
+         for (var i = 0; i < numConnectedWords; i ++){
+            scene.add(connectedWords[i]);
+         }
+      }
+   }
+
+   if (state === 2  && (camera.position.z + camera.position.z * 0.01) < optimalZ){
+      camera.position.z += camera.position.z * 0.01;
+      camera.lookAt( scene.position );
+      if ((camera.position.z + camera.position.z * 0.01) >= optimalZ)
+         state++;
+   }
+
+   if (state === 3){
+      addConnectingLines(numConnectingLines);
+      for (var i = 0; i < numConnectingLines; i ++){
+         scene.add(connectingLines[i]);
+      }
+      state++;
+   }
+
+   if (state === 4){
+      for (var i = 0; i < numConnectingLines; i ++){
+         console.log(points[i*2+1]);
+         points[i*2+1] = {x: points[i*2+1].x*2, y: points[i*2+1].y*2, z: 0}
+      }
+      state++;
+   }
     // Draw!
     renderer.render(scene, camera);
     count += 0.1;
+    //console.log ("count: " + count);
+}
+
+function addWord(x, y, z){
+   var word = new THREE.Sprite( material );
+   word.position.x = x;
+   word.position.y = y;
+   if (z !== undefined)  word.position.z = z;
+   //word.rotation.x = Math.random() * 6;
+   //word.rotation.y = Math.random() * 6;
+   //word.rotation.z = Math.random() * 6;
+   return word;
 }
 
 function generateSprite() {
@@ -146,21 +168,40 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
     renderer.setSize( WIDTH, HEIGHT );
 }
-function onDocumentMouseMove( event ) {
-    mouseX = event.clientX - windowHalfX;
-    mouseY = event.clientY - windowHalfY;
+
+function addConnectedWords( numWords ){
+   var l = 5; // line length
+   if (numWords === undefined) numWords = 8;
+   var angle = 360/numWords;
+   connectedWords = new Array();
+
+   var index = 0;
+
+   for (var i = 0; i < numWords; i ++){
+      var a = angle*i;
+      var x = l * Math.cos(a*Math.PI/180);
+      var y = l * Math.sin(a*Math.PI/180);
+      var newWord = connectedWords [index++]= addWord(x, y, 0);
+      points.push(word.position);
+      points.push(newWord.position);
+   }
+
 }
-function onDocumentTouchStart( event ) {
-    if ( event.touches.length === 1 ) {
-        event.preventDefault();
-        mouseX = event.touches[ 0 ].pageX - windowHalfX;
-        mouseY = event.touches[ 0 ].pageY - windowHalfY;
-    }
-}
-function onDocumentTouchMove( event ) {
-    if ( event.touches.length === 1 ) {
-        event.preventDefault();
-        mouseX = event.touches[ 0 ].pageX - windowHalfX;
-        mouseY = event.touches[ 0 ].pageY - windowHalfY;
-    }
+
+function addConnectingLines (numLines) {
+   var l = 5; // line length
+   if (numLines === undefined) numLines = 8;
+   var angle = 360/numLines;
+   connectingLines = new Array();
+
+   geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+   var index = 0;
+
+   for (var i = 0; i < numLines; i ++){
+      var a = angle*i;
+      var x = l * Math.cos(a*Math.PI/180);
+      var y = l * Math.sin(a*Math.PI/180);
+      var line = connectingLines [index++] = new THREE.Line( geometry, new THREE.LineBasicMaterial( { color: 0xffffff, opacity: 0.5 } ) );
+   }
 }
