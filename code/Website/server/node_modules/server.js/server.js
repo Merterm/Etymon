cde11d47e3107c47e23500328b48e691f3f243ef
Merterm <mertincek@hotmail.com>
2018-05-02 17:04:38 +0300

    server update to run hallucination on cloud

---
+++ code/Website/server/node_modules/server.js/server.js
@@ -0,0 +1,16 @@
+console.log("hello world");
+var a = "matza";
+var number = 26;
+var isMale = true;
+
+
+var  myFunc = function(){
+	
+	console.log("my name is "+a+" i am "+number+" ");
+	
+	
+	
+	
+}
+
+myFunc();
--- code/Website/server/server.js
+++ code/Website/server/server.js
@@ -1,9 +1,16 @@
 let sys = require('util')
 let exec = require('child_process').exec;
+let virtualenv = require('python-virtualenv');
 let child;
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+// Install a virtualenv for the current project.
+virtualenv.installEnv();
+virtualenv.installPackage('numpy');
+virtualenv.installPackage('scipy');
+virtualenv.installPackage('tensorflow');
+virtualenv.installPackage('keras');
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
@@ -45,9 +52,22 @@ app.post('/lstm', function (req, res) {
   console.log(req.body.word);
   // variables
   let word = req.body.word || "closet";
-  let bash_cmnd = "cd LSTM; source ./tensorflow/bin/activate; cd trainer; python generate_text.py \"" + word + "\"; deactivate; cd ../..";
+  //let bash_cmnd = "cd LSTM; source ./tensorflow/bin/activate; cd trainer; python generate_text.py \"" + word + "\"; deactivate; cd ../..";
 
-  // executes bash command
+  // Execute the python script inside the virtualenv
+  virtualenv.executeScript('LSTM/trainer/generate_text.py ' + word).then(
+    function successHandler(stdout){
+      console.log(stdout);
+      var halluc_word = stdout.split('\n');
+      res.send({etymology: "eng: " + word + "\trel:etymology\t" + halluc_word[0], error: null});
+    },
+    function errorHandler(stderr){
+      console.log(stderr);
+      res.send({etymology: null, error: error});
+    }
+  );
+
+  /*// executes bash command
   child = exec(bash_cmnd, function (error, stdout, stderr) {
     if (error !== null) {
       res.send({etymology: null, error: error});
@@ -57,7 +77,7 @@ app.post('/lstm', function (req, res) {
       console.log("eng: " + word + "\trel:etymology\t" + halluc_word[0])
       res.send({etymology: "eng: " + word + "\trel:etymology\t" + halluc_word[0], error: null});
     }
-  });
+  });*/
 })
 
 app.listen(8080, function () {
