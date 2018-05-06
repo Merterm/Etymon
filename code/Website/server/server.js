54f3f2b0d94af37f1323c5463ea5ff090e30d304
Merterm <mertincek@hotmail.com>
2018-05-06 19:55:26 +0300

    New Python Virtualenv Support in Node Try

--- code/Website/server/server.js
+++ code/Website/server/server.js
@@ -1,7 +1,11 @@
 let sys = require('util')
 let exec = require('child_process').exec;
-let virtualenv = require('python-virtualenv');
+let python_virtualenv = require('python-virtualenv');
 var http = require('http'); // 3. HTTP server
+
+var virtualenv = require("virtualenv");
+var packagePath = require.resolve("./package.json")
+var env = virtualenv(packagePath);
 
 let child;
 const express = require('express')
@@ -9,11 +13,11 @@ const app = express()
 const bodyParser = require('body-parser');
 
 // Install a virtualenv for the current project.
-virtualenv.installEnv();
-virtualenv.installPackage('numpy');
-virtualenv.installPackage('scipy');
-virtualenv.installPackage('tensorflow');
-virtualenv.installPackage('keras');
+python_virtualenv.installEnv();
+python_virtualenv.installPackage('numpy');
+python_virtualenv.installPackage('scipy');
+python_virtualenv.installPackage('tensorflow');
+python_virtualenv.installPackage('keras');
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
@@ -61,10 +65,24 @@ app.post('/lstm', function (req, res) {
   console.log(req.body.word);
   // variables
   let word = req.body.word || "closet";
-  let bash_cmnd = "cd LSTM; source ./tensorflow/bin/activate; cd trainer; python generate_text.py \"" + word + "\"; deactivate; cd ../..";
+  //let bash_cmnd = "cd LSTM; source ./tensorflow/bin/activate; cd trainer; python generate_text.py \"" + word + "\"; deactivate; cd ../..";
+
+  //Another take on virtualenv
+  // This is a child_process running Python using your virtualenv. You can
+  // communicate with it over stdin/stdout, etc.
+  var child = env.spawnPython(["./LSTM/trainer/generate_text.py"]);
+  child.stdout.on('data', (data) => {
+    console.log(`child stdout:\n${data}`);
+    var halluc_word = `${data}`.split('\n');
+    res.send({etymology: "eng: " + word + "\trel:etymology\t" + halluc_word[0], error: null});
+  });
+
+  child.stderr.on('data', (data) => {
+    console.error(`child stderr:\n${data}`);
+  });
 
   // Execute the python script inside the virtualenv
-  /*virtualenv.executeScript('LSTM/trainer/generate_text.py ' + word).then(
+  /*python_virtualenv.executeScript('LSTM/trainer/generate_text.py ' + word).then(
     function successHandler(stdout){
       console.log(stdout);
       var halluc_word = stdout.split('\n');
@@ -77,7 +95,7 @@ app.post('/lstm', function (req, res) {
   );*/
 
   // executes bash command
-  child = exec(bash_cmnd, function (error, stdout, stderr) {
+  /*child = exec(bash_cmnd, function (error, stdout, stderr) {
     if (error !== null) {
       res.send({etymology: null, error: error});
     }
@@ -86,7 +104,7 @@ app.post('/lstm', function (req, res) {
       console.log("eng: " + word + "\trel:etymology\t" + halluc_word[0])
       res.send({etymology: "eng: " + word + "\trel:etymology\t" + halluc_word[0], error: null});
     }
-  });
+  });*/
 })
 
 /**
